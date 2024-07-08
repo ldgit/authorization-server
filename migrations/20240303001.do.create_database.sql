4e8bab009966aba3a4f82970dfc7d2b05f0491b9
Danko Lučić <ld.is.a.git@gmail.com>
2024-07-09 01:55:03 +0200

    minor: make client names unique in database

--- migrations/20240303001.do.create_database.sql
+++ migrations/20240303001.do.create_database.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS clients (
   id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  name text NOT NULL,
+  name text NOT NULL UNIQUE,
   description text NOT NULL,
   secret text NOT NULL,
   redirect_uri text NOT NULL
